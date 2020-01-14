3bdb4c7af70969611f2af8872ddc4511fa93641e
David Leblanc <David.LeBlanc@hamiltoncompany.com>
2020-01-14 15:06:44 -0800

    Added basic implementations

---
+++ GimmeMillions.DataAccess/Articles/NYTArticleAccessService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using CSharpFunctionalExtensions;
+using GimmeMillions.Domain.Articles;
+
+namespace GimmeMillions.DataAccess.Articles
+{
+    public class NYTArticleAccessService : IArticleAccessService
+    {
+
+        public Result<IEnumerable<Article>> GetArticles(DateTime dateTime)
+        {
+            var articles = new List<Article>();
+            return Result.Ok<IEnumerable<Article>>(articles);
+        }
+    }
+}
