d7585467128743365c2b670e75870c9aaa81a9fa
David Leblanc <David.LeBlanc@hamiltoncompany.com>
2020-01-14 10:31:31 -0800

    Added Repo and DataAccess Layers

---
+++ GimmeMillions.Domain/Articles/IArticleAccessService.cs
@@ -0,0 +1,14 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimmeMillions.Domain.Articles
+{
+    public interface IArticleAccessService
+    {
+        Result<IEnumerable<Article>> GetArticles(DateTime dateTime);
+    }
+}
---
+++ GimmeMillions.Domain/Articles/IArticleRepository.cs
@@ -0,0 +1,13 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+
+namespace GimmeMillions.Domain.Articles
+{
+    public interface IArticleRepository
+    {
+        Result AddOrUpdate(Article article);
+        Result<IEnumerable<Article>> GetArticles(DateTime dateTime);
+        bool ContainsArticles(DateTime dateTime);
+    }
+}
