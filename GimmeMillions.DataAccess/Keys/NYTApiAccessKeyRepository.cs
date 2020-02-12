d63ce7f28138a77db277cee3d2258a5e6a0b8457
David Leblanc <David.LeBlanc@hamiltoncompany.com>
2020-02-12 12:57:15 -0800

    Added NYTApiAccessKeyRepository

---
+++ GimmeMillions.DataAccess/Keys/NYTApiAccessKeyRepository.cs
@@ -0,0 +1,36 @@
+﻿using CSharpFunctionalExtensions;
+using GimmeMillions.Domain.Keys;
+using System;
+using System.Collections.Generic;
+
+namespace GimmeMillions.DataAccess.Keys
+{
+    public class NYTApiAccessKeyRepository : IAccessKeyRepository
+    {
+        private string _pathToKeys;
+        public NYTApiAccessKeyRepository(string pathToKeys)
+        {
+            _pathToKeys = pathToKeys;
+        }
+
+        public Result AddKey(AccessKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AccessKey> GetActiveKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<AccessKey> GetKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result UpdateKeyStatus(string key, string status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GimmeMillions.Domain/Keys/IAccessKeyRepository.cs
@@ -0,0 +1,17 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimmeMillions.Domain.Keys
+{
+    public interface IAccessKeyRepository
+    {
+        Result AddKey(AccessKey key);
+        IEnumerable<AccessKey> GetActiveKeys();
+        Result<AccessKey> GetKey(string key);
+        Result UpdateKeyStatus(string key, string status);
+    }
+}
