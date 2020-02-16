e9196e1c4ddb215a4d420307fe18179fe5a429a7
David Leblanc <lblancdavid@gmail.com>
2020-02-16 09:28:57 -0800

    Added remaining unit tests for the keys

--- Tests/GimmeMillions.DataAccess.Tests/Keys/NYTApiAccessKeyRepositoryTests.cs
+++ Tests/GimmeMillions.DataAccess.Tests/Keys/NYTApiAccessKeyRepositoryTests.cs
@@ -2,6 +2,7 @@
 using GimmeMillions.DataAccess.Keys;
 using GimmeMillions.Domain.Keys;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace GimmeMillions.DataAccess.Tests.Keys
@@ -19,6 +20,36 @@ namespace GimmeMillions.DataAccess.Tests.Keys
             var result = repo.AddOrUpdateKey(new AccessKey("RJMISprGhvdA1gf9sKqaar0B0VR4457I", "q6ty6EmesuBXNjzY", "active"));
             result.IsFailure.Should().BeFalse();
             File.Exists($"{_pathToKeys}/RJMISprGhvdA1gf9sKqaar0B0VR4457I.json").Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShouldGetKeys()
+        {
+            Directory.Exists(_pathToKeys).Should().BeTrue();
+            var repo = new NYTApiAccessKeyRepository(_pathToKeys);
+
+            var results = repo.GetKeys();
+            results.Count().Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void ShouldGetActiveKeys()
+        {
+            Directory.Exists(_pathToKeys).Should().BeTrue();
+            var repo = new NYTApiAccessKeyRepository(_pathToKeys);
+
+            var results = repo.GetActiveKeys();
+            results.Count().Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void ShouldGetSpecificKey()
+        {
+            Directory.Exists(_pathToKeys).Should().BeTrue();
+            var repo = new NYTApiAccessKeyRepository(_pathToKeys);
+
+            var result = repo.GetKey("RJMISprGhvdA1gf9sKqaar0B0VR4457I");
+            result.IsSuccess.Should().BeTrue();
         }
     }
 }
