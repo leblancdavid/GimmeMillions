c1a5209e96048c70a0f43d63c6f321eab3e968d2
David Leblanc <lblancdavid@gmail.com>
2020-02-17 11:20:59 -0800

    Added stock data in domain

---
+++ GimmeMillions.Domain/Stocks/StockData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GimmeMillions.Domain.Stocks
+{
+    public class StockData
+    {
+        public string Symbol { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public decimal AdjustedClose { get; set; }
+        public decimal PercentDayChange
+        {
+            get
+            {
+                return (decimal)100.0 * (Close - Open) / Open;
+            }
+        }
+
+        public StockData(string symbol, DateTime date, 
+            decimal open, decimal high, decimal low, decimal close, decimal adjClose)
+        {
+            Symbol = symbol;
+            Date = date;
+            Open = open;
+            High = high;
+            Low = low;
+            Close = close;
+            AdjustedClose = adjClose;
+        }
+    }
+}
