9b4d1c432a654faaf6b7feefa2ea96e3d90d605f
David Leblanc <David.LeBlanc@hamiltoncompany.com>
2020-02-19 11:02:14 -0800

    Added prediction models

---
+++ GimmeMillions.Domain/ML/IStockPredictionModel.cs
@@ -0,0 +1,20 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimmeMillions.Domain.ML
+{
+    public interface IStockPredictionModel
+    {
+        string StockSymbol { get; }
+        bool IsTrained { get; }
+        Result<TrainingResult> Train(string symbol, DateTime startDate, DateTime endDate);
+        Result<StockPrediction> Predict(DateTime date);
+        Result<StockPrediction> PredictLatest();
+        Result Save(string pathToModel);
+        Result Load(string pathToModel, string symbol);
+    }
+}
---
+++ GimmeMillions.Domain/ML/IStockPredictionService.cs
@@ -0,0 +1,16 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimmeMillions.Domain.ML
+{
+    public interface IStockPredictionService
+    {
+        IStockPredictionModel Model { get; set; }
+        Result<StockPrediction> GetLatestPrediction();
+
+    }
+}
