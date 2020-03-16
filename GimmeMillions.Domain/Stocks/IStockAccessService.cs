f5273b2336e0296a21339a4c1b653e943facd1c5
David Leblanc <lblancdavid@gmail.com>
2020-03-16 12:08:49 -0700

    Implemented an access service for getting latest stocks

---
+++ GimmeMillions.Domain/Stocks/IStockAccessService.cs
@@ -0,0 +1,11 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+
+namespace GimmeMillions.Domain.Stocks
+{
+    public interface IStockAccessService
+    {
+        IEnumerable<StockData> UpdateStocks(string symbol, DateTime startDate, DateTime endDate);
+    }
+}
