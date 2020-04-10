057c25356789aaaf7af559e668e67c1d769167a6
David Leblanc <lblancdavid@gmail.com>
2020-04-10 08:32:11 -0700

    Simplified features are pretty bad

--- Tests/GimmeMillions.Domain.Tests/ML/MLStockFastForestCandlestickModelTests.cs
+++ Tests/GimmeMillions.Domain.Tests/ML/MLStockFastForestCandlestickModelTests.cs
@@ -44,8 +44,9 @@ namespace GimmeMillions.Domain.Tests.ML
             var stocksRepo = new YahooFinanceStockAccessService(new StockDataRepository(_pathToStocks), _pathToStocks);
 
             var cache = new FeatureJsonCache<FeatureVector>(_pathToCache);
-            var featureExtractor = new CandlestickStockFeatureExtractorV2();
-            int numberSamples = 30;
+            //var featureExtractor = new CandlestickStockFeatureExtractorV2();
+            var featureExtractor = new CandlestickSimplifiedStockFeatureExtractor();
+            int numberSamples = 20;
             return new CandlestickStockFeatureDatasetService(featureExtractor, stocksRepo, cache, numberSamples);
         }
     }
