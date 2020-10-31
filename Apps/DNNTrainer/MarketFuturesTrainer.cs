292ce7671b1fde7b1d278cc8da56ddba6b508ef4
David Leblanc <lblancdavid@gmail.com>
2020-10-31 08:02:32 -0700

    Added the market futures model

--- Apps/DNNTrainer/MarketFuturesTrainer.cs
+++ Apps/DNNTrainer/MarketFuturesTrainer.cs
@@ -13,24 +13,7 @@ using System.Linq;
 
 namespace DNNTrainer
 {
-    public class PercentDayChangeOutputMapper : ITrainingOutputMapper
-    {
-        private decimal _percentChangeThreshold;
-        public PercentDayChangeOutputMapper(decimal percentChangeThreshold)
-        {
-            _percentChangeThreshold = percentChangeThreshold;
-        }
-
-        public bool GetBinaryValue(StockData stockData)
-        {
-            return stockData.PercentChangeFromPreviousClose > _percentChangeThreshold;
-        }
-
-        public float GetOutputValue(StockData stockData)
-        {
-            return (float)stockData.PercentChangeFromPreviousClose;
-        }
-    };
+   
     public class MarketFuturesTrainer
     {
         private IStockRepository _stockRepository;
@@ -41,14 +24,8 @@ namespace DNNTrainer
         }
         public void Train(string modelFile)
         {
-            //var datasetService = GetCandlestickFeatureDatasetService(60, 5, true);
             var datasetService = GetCandlestickFeatureDatasetServiceV3(20, 5, 5);
 
-            //var model = new MLStockFastForestCandlestickModel();
-            //model.Parameters.NumCrossValidations = 2;
-            //model.Parameters.NumOfTrees = 2000;
-            //model.Parameters.NumOfLeaves = 40;
-            //model.Parameters.MinNumOfLeaves = 5;
             var model = new MLStockRangePredictorModel();
 
             var trainingData = new List<(FeatureVector Input, StockData Output)>();
@@ -69,18 +46,6 @@ namespace DNNTrainer
 
             var trainingResults = model.Train(trainingData, 0.0, new PercentDayChangeOutputMapper(averageGrowth));
             model.Save(modelFile);
-
-            var diaSamples = datasetService.GetFeatures("DIA").Where(x => x.Date > new DateTime(2020, 1, 1));
-            using (System.IO.StreamWriter file =
-            new System.IO.StreamWriter($"C:\\Stocks\\dia-test.txt"))
-            {
-                foreach (var sample in diaSamples)
-                {
-                    var prediction = model.Predict(sample);
-                    //file.WriteLine($"{sample.Date}\t{prediction.Sentiment}\t{prediction.PredictedHigh}\t{prediction.PredictedLow}");
-                    file.WriteLine($"{sample.Date}\t{prediction.Probability}");
-                }
-            }
         }
 
         private IFeatureDatasetService<FeatureVector> GetCandlestickFeatureDatasetServiceV3(
@@ -90,13 +55,6 @@ namespace DNNTrainer
         {
             var stocksRepo = new YahooFinanceStockAccessService(_stockRepository);
             var extractor = new RawStockFeatureExtractor();
-            //var extractor = new StockIndicatorsFeatureExtractionV3(timesampling,
-            //    numStockSamples, //BOLL
-            //    (int)(numStockSamples * 0.8), (int)(numStockSamples * 0.4), (int)(numStockSamples * 0.3), 5, //MACD
-            //    (int)(numStockSamples), 5, //VWAP
-            //    (int)(numStockSamples), 5, //RSI
-            //    (int)(numStockSamples), 5, //CMF
-            //    5);
             return new BuySellSignalFeatureDatasetService(extractor, stocksRepo,
                 numStockSamples, kernelSize);
         }
