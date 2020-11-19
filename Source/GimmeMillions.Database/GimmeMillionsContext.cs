3f853e6c77d5fce9bb2c0f9ab8afc500cbf5413a
David Leblanc <lblancdavid@gmail.com>
2020-11-19 10:16:41 -0800

    Added the Database project

---
+++ Source/GimmeMillions.Database/GimmeMillionsContext.cs
@@ -0,0 +1,37 @@
+﻿using GimmeMillions.Domain.Features;
+using GimmeMillions.Domain.Stocks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GimmeMillions.SQLDataAccess
+{
+    public class GimmeMillionsContext : DbContext
+    {
+        public DbSet<FeatureVector> FeatureVectors { get; set; }
+        public DbSet<StockHistory> StockHistories { get; set; }
+        public DbSet<StockRecommendation> StockRecommendations { get; set; }
+
+        public GimmeMillionsContext(DbContextOptions<GimmeMillionsContext> options)
+            : base(options)
+        { }
+
+        public GimmeMillionsContext()
+        { }
+        //Uncomment this when you want to add a migration? Not sure why but it's apparently necessary
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //    => options.UseSqlite("DataSource=default.db");
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FeatureVector>()
+                .Ignore(x => x.Data)
+                .Property<string>("DataStr");
+
+            modelBuilder.Entity<StockHistory>().Ignore(x => x.HistoricalData);
+        }
+
+
+
+    }
+}
---
+++ Source/GimmeMillions.Database/Migrations/20200820031006_InitialDb.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GimmeMillions.SQLDataAccess.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FeatureVectors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Encoding = table.Column<string>(nullable: true),
+                    DataStr = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FeatureVectors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StockHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Symbol = table.Column<string>(nullable: true),
+                    HistoricalDataStr = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockHistories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FeatureVectors");
+
+            migrationBuilder.DropTable(
+                name: "StockHistories");
+        }
+    }
+}
