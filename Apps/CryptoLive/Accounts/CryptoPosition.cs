7c0923d7e278c61930d37a752de9efb495c8ac83
David Leblanc <lblancdavid@gmail.com>
2020-11-24 07:58:09 -0800

    Fixed a bug when saving and loading accounts

--- Apps/CryptoLive/Accounts/CryptoPosition.cs
+++ Apps/CryptoLive/Accounts/CryptoPosition.cs
@@ -6,16 +6,21 @@ namespace CryptoLive.Accounts
 {
     public class CryptoPosition
     {
-        public string Symbol { get; private set; }
-        public decimal PricePaid { get; private set; }
-        public DateTime TransactionDate { get; private set; }
-        public decimal PositionSize { get; private set; }
+        public string Symbol { get; set; }
+        public decimal PricePaid { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public decimal PositionSize { get; set; }
         public decimal PositionValue
         {
             get
             {
                 return PositionSize * PricePaid;
             }
+        }
+
+        public CryptoPosition()
+        {
+            TransactionDate = DateTime.UtcNow;
         }
 
         public CryptoPosition(string symbol, decimal price, decimal size)
---
+++ Tests/CryptoLive.Tests/SimulatedCryptoAccountManagerTests.cs
@@ -0,0 +1,56 @@
+﻿using CryptoLive.Accounts;
+using CryptoLive.Notification;
+using GimmeMillions.Domain.Stocks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CryptoLive.Tests
+{
+    public class SimulatedCryptoAccountManagerTests
+    {
+        [Fact]
+        public void TestingBuyAndSellNotifications()
+        {
+            var manager = new SimulationCryptoAccountManager();
+            for(int i = 0; i < 10; ++i)
+            {
+                manager.Notify(new CryptoEventNotification(
+                   new StockData("TEST",
+                   DateTime.UtcNow,
+                   10.0m, 10.0m, 10.0m, 10.0m, 10.0m, 10.0m, 10.0m),
+                   95.0));
+
+                manager.Notify(new CryptoEventNotification(
+                   new StockData("TEST",
+                   DateTime.UtcNow,
+                   20.0m, 20.0m, 20.0m, 20.0m, 20.0m, 20.0m, 20.0m),
+                   5.0));
+
+            }
+            
+        }
+
+        [Fact]
+        public void SaveLoadTest()
+        {
+            var manager = new SimulationCryptoAccountManager();
+            manager.Notify(new CryptoEventNotification(
+                   new StockData("TEST1",
+                   DateTime.UtcNow,
+                   10.0m, 10.0m, 10.0m, 10.0m, 10.0m, 10.0m, 10.0m),
+                   95.0));
+            manager.Notify(new CryptoEventNotification(
+                   new StockData("TEST2",
+                   DateTime.UtcNow,
+                   20.0m, 20.0m, 20.0m, 20.0m, 20.0m, 20.0m, 20.0m),
+                   95.0));
+
+            manager.SaveAccount("TestAccount.json");
+            manager.LoadAccount("TestAccount.json");
+
+            Assert.True(manager.Account.CurrentValue >= 990.0m);
+        }
+    }
+}
