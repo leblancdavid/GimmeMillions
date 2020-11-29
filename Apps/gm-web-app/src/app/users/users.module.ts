dfec4a8466a92a17c7982d2327430b3eded1d4c6
David Leblanc <lblancdavid@gmail.com>
2020-11-28 20:25:22 -0800

    Added the material module and users module

---
+++ Apps/gm-web-app/src/app/users/users.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '../material.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MaterialModule
+  ]
+})
+export class UsersModule { }
