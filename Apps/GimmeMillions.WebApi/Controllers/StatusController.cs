a2c11b5debf32bb53a2da60b15655198b0fe9798
David Leblanc <lblancdavid@gmail.com>
2020-12-28 18:37:13 -0800

    Added a status call

---
+++ Apps/GimmeMillions.WebApi/Controllers/StatusController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GimmeMillions.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStatus()
+        {
+            return Ok("Gimmillions API Running...");
+        }
+    }
+}
