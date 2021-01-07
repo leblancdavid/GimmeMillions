fffd8014804f25212c9c8e07962cda4b0bc32766
David Leblanc <lblancdavid@gmail.com>
2021-01-06 18:06:50 -0800

    Added divider between notification messages

--- Apps/gm-web-app/src/app/notifications/notifications.service.ts
+++ Apps/gm-web-app/src/app/notifications/notifications.service.ts
@@ -8,13 +8,15 @@ import { NotificationComponent } from './notification/notification.component';
 })
 export class NotificationsService {
 
-  constructor(private _snackBar: MatSnackBar) { 
+  constructor(private _snackBar: MatSnackBar) {
     this.currentNotifications = new Array<Notification>();
     this.currentNotifications.push(new Notification(`Welcome to the <b>Gimmillions</b> app! We are still very
     early in development but we appreciate your support and time. All feedback is welcomed and is very valuable to us.
     If you have ideas for improvement or find bugs, please sign up and submit feedback/enhancement ideas/bugs
     <a href="https://github.com/leblancdavid/GimmeMillions/issues" class="light-accent-text"
             target="_blank">here</a>`));
+
+    this.currentNotifications.push(new Notification(`Just another notification for testing purposes`));
   }
 
   public currentNotifications: Array<Notification>;
