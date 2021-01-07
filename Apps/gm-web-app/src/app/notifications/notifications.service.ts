90f7ba4f1375eb04b74601652b66847185e61e84
David Leblanc <lblancdavid@gmail.com>
2021-01-06 17:59:21 -0800

    Added the snack-bar component for notifications

--- Apps/gm-web-app/src/app/notifications/notifications.service.ts
+++ Apps/gm-web-app/src/app/notifications/notifications.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Notification } from './notification';
+import { NotificationComponent } from './notification/notification.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
 
-  constructor() { 
+  constructor(private _snackBar: MatSnackBar) { 
     this.currentNotifications = new Array<Notification>();
-    this.currentNotifications.push(new Notification(`Welcome to the <b>Gimmillions</b> app!`));
+    this.currentNotifications.push(new Notification(`Welcome to the <b>Gimmillions</b> app! We are still very
+    early in development but we appreciate your support and time. All feedback is welcomed and is very valuable to us.
+    If you have ideas for improvement or find bugs, please sign up and submit feedback/enhancement ideas/bugs
+    <a href="https://github.com/leblancdavid/GimmeMillions/issues" class="light-accent-text"
+            target="_blank">here</a>`));
   }
 
   public currentNotifications: Array<Notification>;
+
+  public show() {
+    this._snackBar.openFromComponent(NotificationComponent, {
+    });
+  }
 }
