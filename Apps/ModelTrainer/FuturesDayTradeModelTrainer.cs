8a1d1de3abcdb4fd6496a7fa37d95958754a295f
David Leblanc <lblancdavid@gmail.com>
2021-03-01 11:34:59 -0800

    Added a new 15m day trading model

--- Apps/ModelTrainer/FuturesDayTradeModelTrainer.cs
+++ Apps/ModelTrainer/FuturesDayTradeModelTrainer.cs
@@ -25,18 +25,18 @@ namespace ModelTrainer
         }
         public void Train(string modelName, StockDataPeriod period)
         {
-            var datasetService = GetRawFeaturesBuySellSignalDatasetService(period, 50, 9);
-            //var datasetService = GetIndicatorFeaturesBuySellSignalDatasetService(period, 12, 80, 9);
+            //var datasetService = GetRawFeaturesBuySellSignalDatasetService(period, 50, 9);
+            var datasetService = GetIndicatorFeaturesBuySellSignalDatasetService(period, 12, 80, 9);
             var model = new MLStockRangePredictorModel();
 
-            int numSamples = 20000;
+            int numSamples = 50000;
             var trainingData = new List<(FeatureVector Input, StockData Output)>();
             trainingData.AddRange(datasetService.GetTrainingData("DIA", null, true, numSamples));
             trainingData.AddRange(datasetService.GetTrainingData("SPY", null, true, numSamples));
             trainingData.AddRange(datasetService.GetTrainingData("QQQ", null, true, numSamples));
             trainingData.AddRange(datasetService.GetTrainingData("RUT", null, true, numSamples));
 
-            model.Train(trainingData, 0.1, new SignalOutputMapper());
+            model.Train(trainingData, 0.0, new SignalOutputMapper());
             model.Save(modelName);
         }
 
--- Apps/ModelTrainer/Program.cs
+++ Apps/ModelTrainer/Program.cs
@@ -23,7 +23,7 @@ namespace ModelTrainer
                 new StockSymbolsFile("nasdaq_screener.csv"),
                 "I12BJE0PV9ARIGTWWOPJGCGRWPBUJLRP");
 
-            trainer.Train("Resources/Models/EgyptianMau/DayTrader_15m", StockDataPeriod.FifteenMinute);
+            trainer.Train("C:\\Users\\leblanc_d\\Documents\\Projects\\GimmeMillions\\Repository\\Models\\EgyptianMau\\DayTrader_15m", StockDataPeriod.FifteenMinute);
 
             //var trainer = new EgyptianMauModelTrainer(new DefaultStockRepository(stockSqlDb), 
             //    new StockSymbolsFile("nasdaq_screener.csv"),
