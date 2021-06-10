31644925f935eb73fa45574dd7a23d1133434d94
David Leblanc <lblancdavid@gmail.com>
2021-06-10 16:00:54 -0700

    Updated the style for the dots on the charts

--- Apps/gm-web-app/src/app/stocks/sentiment-history-chart/sentiment-history-chart.component.ts
+++ Apps/gm-web-app/src/app/stocks/sentiment-history-chart/sentiment-history-chart.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MatSelectChange } from '@angular/material/select';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
+import { StockRecommendation } from '../stock-recommendation';
 import { StockRecommendationHistory } from '../stock-recommendation-history';
 
 @Component({
@@ -55,18 +56,28 @@ export class SentimentHistoryChartComponent implements OnInit {
     if(this.history) {
       const sortedR = this.history.historicalData.sort((n1,n2)=> n1.date.getUTCDate() - n2.date.getUTCDate());
       let data = [];
+      let borders = [];
+      let backgrounds = [];
       this.lineChartColors = [
         {
-          borderColor: '#1b603a',
-          backgroundColor: 'rgba(196,210,83,0.25)',
+          borderColor: this.history.lastRecommendation.getHsl(25),
+          backgroundColor: this.history.lastRecommendation.getHsl(90),
+          pointBorderWidth: 2,
+          pointRadius: 4
         }
       ];
       this.sentimentAxis = [];
       this.labelAxis = [];
+      
       for(let i = 0; i < sortedR.length && this.maxLength; ++i) {
+        borders.push(sortedR[i].getHsl(25));
+        backgrounds.push(sortedR[i].getHsl(70));
         data.push(sortedR[i].sentiment);
         this.labelAxis.push((sortedR[i].date.getMonth() + 1).toString() + '/' + sortedR[i].date.getDate().toString());
       }
+
+      this.lineChartColors[0].pointBackgroundColor = backgrounds;
+      this.lineChartColors[0].pointBorderColor = borders;
 
       this.sentimentAxis.push({
         data: data, label: 'Sentiment'
@@ -78,18 +89,27 @@ export class SentimentHistoryChartComponent implements OnInit {
     if(this.history) {
       const sortedR = this.history.historicalData.sort((n1,n2)=> n1.date.getUTCDate() - n2.date.getUTCDate());
       let data = [];
+      let borders = [];
+      let backgrounds = [];
       this.lineChartColors = [
         {
-          borderColor: '#cb5115',
-          backgroundColor: 'rgba(203,81,21,0.25)',
+          borderColor: this.history.lastRecommendation.getHsl(25),
+          backgroundColor: this.history.lastRecommendation.getHsl(90),
+          pointBorderWidth: 2,
+          pointRadius: 4
         }
       ];
       this.sentimentAxis = [];
       this.labelAxis = [];
       for(let i = 0; i < sortedR.length && this.maxLength; ++i) {
+        borders.push(sortedR[i].getHsl(25));
+        backgrounds.push(sortedR[i].getHsl(70));
         data.push(sortedR[i].confidence);
         this.labelAxis.push((sortedR[i].date.getMonth() + 1).toString() + '/' + sortedR[i].date.getDate().toString());
       }
+
+      this.lineChartColors[0].pointBackgroundColor = backgrounds;
+      this.lineChartColors[0].pointBorderColor = borders;
 
       this.sentimentAxis.push({
         data: data, label: 'Confidence'
@@ -101,19 +121,28 @@ export class SentimentHistoryChartComponent implements OnInit {
     if(this.history) {
       const sortedR = this.history.historicalData.sort((n1,n2)=> n1.date.getUTCDate() - n2.date.getUTCDate());
       let s = [];
+      let borders = [];
+      let backgrounds = [];
       this.lineChartColors = [
         {
-          borderColor: 'black',
-          backgroundColor: 'rgba(0,0,0,0.25)',
+          borderColor: this.history.lastRecommendation.getHsl(25),
+          backgroundColor: this.history.lastRecommendation.getHsl(90),
+          pointBorderWidth: 2,
+          pointRadius: 4
         }
       ];
       this.sentimentAxis = [];
       this.labelAxis = [];
       for(let i = 0; i < sortedR.length && this.maxLength; ++i) {
+        borders.push(sortedR[i].getHsl(25));
+        backgrounds.push(sortedR[i].getHsl(70));
         s.push(sortedR[i].lastData.close);
         this.labelAxis.push((sortedR[i].date.getMonth() + 1).toString() + '/' + sortedR[i].date.getDate().toString());
       }
 
+      this.lineChartColors[0].pointBackgroundColor = backgrounds;
+      this.lineChartColors[0].pointBorderColor = borders;
+      
       this.sentimentAxis.push({
         data: s, label: 'Price'
       });
