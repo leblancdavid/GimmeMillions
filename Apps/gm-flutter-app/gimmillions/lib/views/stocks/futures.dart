054ee75a69950de731673a240aee5e33c6a39188
David Leblanc <lblancdavid@gmail.com>
2021-06-18 15:04:28 -0700

    added futures widget

---
+++ Apps/gm-flutter-app/gimmillions/lib/views/stocks/futures.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FuturesWidget extends StatefulWidget {
+  @override
+  _FuturesState createState() => _FuturesState();
+}
+
+class _FuturesState extends State<FuturesWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
