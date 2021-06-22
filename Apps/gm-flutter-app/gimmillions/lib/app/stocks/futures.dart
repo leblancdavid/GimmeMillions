cc7e0b7f3036cab0535e762ee511167c7d46fde5
David Leblanc <lblancdavid@gmail.com>
2021-06-22 15:32:03 -0700

    Got the refresh functionality working

--- Apps/gm-flutter-app/gimmillions/lib/app/stocks/futures.dart
+++ Apps/gm-flutter-app/gimmillions/lib/app/stocks/futures.dart
@@ -1,57 +1,119 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:gimmillions/models/stock-recommendation.dart';
+import 'package:gimmillions/services/stock-recommendation-service.dart';
+import 'package:provider/provider.dart';
 
 class FuturesWidget extends StatefulWidget {
   @override
   _FuturesState createState() => _FuturesState();
 }
 
+enum _FuturesMenuOptions {
+  refresh,
+}
+
 class _FuturesState extends State<FuturesWidget> {
+  late Future<List<StockRecommendation>> _futuresList;
+
+  _refreshFutures(BuildContext context) {
+    try {
+      final service =
+          Provider.of<StockRecommendationService>(context, listen: false);
+      _futuresList = service.getFutures();
+      setState(() {});
+    } catch (e) {
+      print(e);
+      return List.empty();
+    }
+  }
+
+  late Future<List<StockRecommendation>> futuresList;
+
   @override
   Widget build(BuildContext context) {
-    return DataTable(
-      columns: const <DataColumn>[
-        DataColumn(
-          label: Text(
-            'Name',
-            style: TextStyle(fontStyle: FontStyle.italic),
-          ),
-        ),
-        DataColumn(
-          label: Text(
-            'Age',
-            style: TextStyle(fontStyle: FontStyle.italic),
-          ),
-        ),
-        DataColumn(
-          label: Text(
-            'Role',
-            style: TextStyle(fontStyle: FontStyle.italic),
-          ),
-        ),
-      ],
-      rows: const <DataRow>[
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Sarah')),
-            DataCell(Text('19')),
-            DataCell(Text('Student')),
-          ],
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Janine')),
-            DataCell(Text('43')),
-            DataCell(Text('Professor')),
-          ],
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('William')),
-            DataCell(Text('27')),
-            DataCell(Text('Associate Professor')),
-          ],
-        ),
-      ],
-    );
+    _refreshFutures(context);
+
+    return Container(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          PopupMenuButton<_FuturesMenuOptions>(
+              onSelected: (_FuturesMenuOptions result) {
+                if (result == _FuturesMenuOptions.refresh) {
+                  _refreshFutures(context);
+                }
+              },
+              itemBuilder: (BuildContext context) =>
+                  <PopupMenuEntry<_FuturesMenuOptions>>[
+                    PopupMenuItem(
+                        value: _FuturesMenuOptions.refresh,
+                        child: Row(
+                          children: [Icon(Icons.refresh), Text("Refresh")],
+                        ))
+                  ]),
+          FuturesDataTableBuilder(_futuresList, _refreshFutures)
+        ]));
+  }
+}
+
+class FuturesDataTableBuilder extends StatelessWidget {
+  final Future<List<StockRecommendation>> _futuresList;
+  final Function onRefresh;
+
+  const FuturesDataTableBuilder(this._futuresList, this.onRefresh);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _futuresList,
+        builder: (BuildContext context,
+            AsyncSnapshot<List<StockRecommendation>> snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return CircularProgressIndicator(
+                color: Theme.of(context).primaryColor);
+          }
+
+          if (snapshot.hasData) {
+            return DataTable(columns: const <DataColumn>[
+              DataColumn(
+                label: Text(
+                  'Symbol',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
+              DataColumn(
+                label: Text(
+                  'Sentiment',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
+              DataColumn(
+                label: Text(
+                  'Confidence',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
+            ], rows: _toTableRows(snapshot.data));
+          }
+
+          return CircularProgressIndicator(
+              color: Theme.of(context).primaryColor);
+        });
+  }
+
+  List<DataRow> _toTableRows(List<StockRecommendation>? futures) {
+    if (futures == null) {
+      return List.empty();
+    }
+
+    return futures
+        .map((e) => DataRow(cells: [
+              DataCell(Text(e.symbol)),
+              DataCell(Text(e.sentiment.toStringAsFixed(2))),
+              DataCell(Text(e.confidence.toStringAsFixed(3))),
+            ]))
+        .toList();
   }
 }
--- Apps/gm-flutter-app/gimmillions/lib/services/stock-recommendation-service.dart
+++ Apps/gm-flutter-app/gimmillions/lib/services/stock-recommendation-service.dart
@@ -3,7 +3,21 @@ import 'package:gimmillions/models/stock-recommendation.dart';
 
 class StockRecommendationService {
   Future<List<StockRecommendation>> getFutures() {
-    List<StockRecommendation> recommendations = List.empty();
+    List<StockRecommendation> recommendations = [];
+
+    recommendations.add(StockRecommendation(
+        DateTime.now(),
+        'DIA',
+        'Test',
+        42.24,
+        0.42,
+        11.11,
+        22.22,
+        33.33,
+        44.44,
+        55.55,
+        StockData(DateTime.now(), 'DIA', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)));
+
     return Future.delayed(Duration(seconds: 2), () => recommendations);
   }
 
